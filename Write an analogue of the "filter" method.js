178e267b8333dad1ecff67e852129b16d21112e9
iris-2008 <155109756+iris-2008@users.noreply.github.com>
2025-02-14 23:47:28 +0300

    Create Write an analogue of the "filter" method.js
    Написать аналог метода "filter"

---
+++ Write an analogue of the "filter" method.js
@@ -0,0 +1,9 @@
+function myFilter(arr, callback) {
+	const filteredArray = []
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			filteredArray.push(arr[i])
+		}
+	}
+	return filteredArray
+}
